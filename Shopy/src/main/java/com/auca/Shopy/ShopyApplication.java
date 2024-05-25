69092701efed662162ed520a1ed339bf9fee0725
jackmagosa <jackmangosa@gmail.com>
2024-05-25 15:09:06 +0200

    Fixed BCryptPasswordEncoder bean error

--- Shopy/src/main/java/com/auca/Shopy/ShopyApplication.java
+++ Shopy/src/main/java/com/auca/Shopy/ShopyApplication.java
@@ -3,9 +3,11 @@ package com.auca.Shopy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.auca.Shopy")
 public class ShopyApplication {
 
 	public static void main(String[] args) {
